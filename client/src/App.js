54df56fdafbcd2a8fe0b72eefb3b1b8ec241aead
ME <mebener44@gmail.com>
2021-08-16 19:55:54 -0700

    initial file setup

---
+++ client/src/App.js
@@ -0,0 +1,70 @@
+// import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+// import React from 'react';
+
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
+// import Home from './pages/Home';
+
+import React from 'react';
+
+import { ApolloProvider } from '@apollo/client';
+import ApolloClient from 'apollo-boost';
+// import { setContext } from '@apollo/client/link/context';
+//apparently not needed? ^^
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+
+// const client = new ApolloClient({
+//   uri: '/graphql'
+// });
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+}); 
+
+
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+export default App;
